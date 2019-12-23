60be381ccba1e892b26da04e729ae41c86c148c6
evgenii-codesmith <evgenii.codesmith@gmail.com>
2019-12-24 00:05:59 +0300

    Send errors via email to admin

--- config.py
+++ config.py
@@ -8,3 +8,9 @@ class Config(object):
         'DATABASE_URL') or 'sqlite:///' + str(pathlib.Path('app.db').resolve())
 
     SQLALCHEMY_TRACK_MODIFICATION = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT' or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['email@example.com']
