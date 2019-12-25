e5605b164e1d46569590118ca2da056cb7d2ff87
evgenii-codesmith <evgenii.codesmith@gmail.com>
2019-12-25 16:51:43 +0300

    Log errors to logfile

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+from pathlib import Path
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -34,3 +35,16 @@ if not app.debug:
         )
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    if Path('logs').exists:
+        Path('logs').mkdir()
+
+    file_handler = RotatingFileHandler(
+        'logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
