ab5dd5b2bb9bc8f3dce85bfbbb1a0298ff22fdcd
evgenii-codesmith <evgenii.codesmith@gmail.com>
2020-01-02 21:25:58 +0300

    Add async email send

--- app/email.py
+++ app/email.py
@@ -2,13 +2,19 @@ from flask_mail import Message
 from app import mail
 from flask import render_template
 from app import app
+from threading import Thread
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 
 def send_password_reset_email(user):
@@ -17,5 +23,5 @@ def send_password_reset_email(user):
                sender=app.config['ADMINS'][0], recipients=[user.email],
                text_body=render_template('email/reset_password.txt', user=user,
                                          token=token),
-               html_body=render_template('email/reset_password.html', user=user,
-                                         toekn=token))
+               html_body=render_template('email/reset_password.html',
+                                         user=user, token=token))
