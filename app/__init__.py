513e48dcf7785d232bd824efc941aea481a18092
evgenii-codesmith <evgenii.codesmith@gmail.com>
2020-01-11 22:59:56 +0300

    refactor(app):Blueprints-init ver

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,50 @@
-
-from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from pathlib import Path
+
+from flask import Flask, current_app, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel
-from flask import request
+from config import Config
 
-app = Flask(__name__)
-mail = Mail(app)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
-babel = Babel(app)
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+login.login_message = 'Please login to access this page'
+mail = Mail()
+bootstrap = Bootstrap()
+moment = Moment()
+babel = Babel()
 
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
 
-from app import routes, models, errors  # noqa E402
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-if not app.debug:
-    if app.config['MAIL_SERVER']:
-        auth = None
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    mail.init_app(app)
+    bootstrap.init_app(app)
+    babel.init_app(app)
+
+    from app.errors import bp as errors_bp  # noqa
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp  # noqa
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+    if not app.debug and not app.testing:
+        if app.config['MAIL_SERVER']:
+            auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
         secure = None
@@ -59,7 +74,12 @@ if not app.debug:
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
 
+    return app
+
 
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+from app import models  # noqa
